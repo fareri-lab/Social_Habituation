d9b1d5aaaa444c9ccca493733c246e885dbe3101
dfareri <dfareri@users.noreply.github.com>
2021-04-24 17:07:10 -0400

    Update L1stats_DF_act_take2.sh

--- code/L1stats_DF_act_take2.sh
+++ code/L1stats_DF_act_take2.sh
@@ -14,7 +14,7 @@
 #maindir="$(dirname "$scriptdir")"
 
 maindir=/Users/farerilab/Documents/Habituation_fMRI/deidentified
-
+mkdir ${maindir}/templates
 # study-specific inputs
 TASK=sonet
 TYPE=act
@@ -29,8 +29,7 @@ TYPE=act
 #fi
 
 # set inputs and general outputs (should not need to chage across studies in Smith Lab)
-MAINOUTPUT=${maindir}/derivatives/fsl/sub-${sub}
-mkdir -p $MAINOUTPUT
+
 
 #DATA=${maindir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-${run}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz
 #CONFOUNDEVS=${maindir}/derivatives/fsl/confounds/sub-${sub}/sub-${sub}_task-${TASK}_run-${run}_desc-fslConfounds.tsv
@@ -38,7 +37,6 @@ mkdir -p $MAINOUTPUT
 #	echo "missing: $CONFOUNDEVS " >> ${maindir}/re-runL1.log
 #	exit # exiting to ensure nothing gets run without confounds
 #fi
-EVDIR=${maindir}/derivatives/fsl/EVfiles/sub-${sub}/${TASK}/run-0${run}
 
 # check for empty EVs (extendable to other studies)
 MISSED_TRIAL_Q=${EVDIR}/Miss_Ques_${run}.txt
@@ -61,7 +59,11 @@ for sub in "01"; do
 		DATA=${maindir}/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-${run}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz
 		CONFOUNDEVS=${maindir}/derivatives/fsl/confounds/sub-${sub}
 		sm=6
-		
+		MAINOUTPUT=${maindir}/derivatives/fsl/sub-${sub}
+		mkdir -p $MAINOUTPUT
+		EVDIR=${maindir}/derivatives/fsl/EVfiles/sub-${sub}/${TASK}/run-0${run}
+
+
 		if [ ! -e $CONFOUNDEVS ]; then
 			echo "missing: $CONFOUNDEVS " >> ${maindir}/re-runL1.log
 			exit # exiting to ensure nothing gets run without confounds
@@ -83,12 +85,12 @@ for sub in "01"; do
 		INPUT01=${EVDIR}/C_Ques_${run}.txt
 		INPUT02=${EVDIR}/H_Ques_${run}.txt
 		INPUT03=${EVDIR}/F_Ques_${run}.txt
-		INPUT05=${EVDIR}/PunishC_Ques_${run}.txt
-		INPUT06=${EVDIR}/PunishH_Ques_${run}.txt
-		INPUT07=${EVDIR}/PunishF_Ques_${run}.txt
-		INPUT08=${EVDIR}/RewardC_Ques_${run}.txt
-		INPUT09=${EVDIR}/RewardH_Ques_${run}.txt
-		INPUT10=${EVDIR}/RewardF_Ques_${run}.txt
+		INPUT04=${EVDIR}/PunishC_Ques_${run}.txt
+		INPUT05=${EVDIR}/PunishH_Ques_${run}.txt
+		INPUT06=${EVDIR}/PunishF_Ques_${run}.txt
+		INPUT07=${EVDIR}/RewardC_Ques_${run}.txt
+		INPUT08=${EVDIR}/RewardH_Ques_${run}.txt
+		INPUT09=${EVDIR}/RewardF_Ques_${run}.txt
 
 		ITEMPLATE=${maindir}/templates/L1_task-${TASK}_model-01_type-${TYPE}.fsf
 		OTEMPLATE=${MAINOUTPUT}/L1_sub-${sub}_task-${TASK}_model-01_run-0${run}.fsf
